344611d709517194292ba0eb19a81dee54ecad54
LilyaOtherside <lilya.ishenko@gmail.com>
2024-12-02 15:01:09 +0200

    fix: исправлена типизация Telegram WebApp

--- src/App.tsx
+++ src/App.tsx
@@ -21,16 +21,21 @@ declare global {
   interface TelegramWebApps {
     WebApp: {
       ready: () => void;
-      initData: string;
-      initDataUnsafe?: {
+      expand: () => void;
+      close: () => void;
+      initDataUnsafe: {
         user?: {
           id: number;
-          username?: string;
-          first_name?: string;
+          first_name: string;
           last_name?: string;
+          username?: string;
+          language_code?: string;
           photo_url?: string;
-        }
-      }
+        };
+        start_param?: string;
+        auth_date: number;
+      };
+      initData: string;
     }
   }
 
